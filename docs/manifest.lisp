84be3bc733702476b1899f2d0da007347708eeeb
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-07 12:02:38 -0300

    Add documentation manifest

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:scriba)
+ :documents ((:title "Scriba"
+              :authors ("Fernando Borretti")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
